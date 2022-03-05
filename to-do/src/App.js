f74e5414bac047fd462604fc03a1064b05bfe4e3
MrBasch <mikhail.baschsmanna@utem.cl>
2022-03-05 02:00:52 -0300

    Implemented the function to filter tasks by status

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -5,9 +5,16 @@ import Todo from './components/todo';
 import { nanoid } from "nanoid";
 
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
   const [tasks, setTasks] = useState(props.task);
+  const [filter, setFilter] = useState('All');
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map(task => {
       // if this task has the same ID as the edited task
@@ -33,7 +40,9 @@ function App(props) {
     const remainingTasks = tasks.filter(task => id !== task.id);
     setTasks(remainingTasks); 
   }
-  const taskList= tasks.map(task => (
+  const taskList= tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
     <Todo 
       id={task.id}
       name={task.name}
@@ -45,6 +54,14 @@ function App(props) {
       />
     )
   );
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   function addTask(name) {
@@ -56,9 +73,7 @@ function App(props) {
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
-      <FilterButton />
-      <FilterButton />
-      <FilterButton />
+        {filterList}
       <h2 id="list-heading">
         {headingText}
       </h2>
